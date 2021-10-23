eac230096588c5428c4065962134c5a8dae75d85
samuelru3 <samuelruebel@gmail.com>
2021-10-23 02:17:58 +0200

    commit

--- lib/ui_windows/result_window.dart
+++ lib/ui_windows/result_window.dart
@@ -5,12 +5,48 @@ class ResultWindow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Ergebnis',
-          style: kAppBarTextStyle,
+        appBar: AppBar(
+          title: Text(
+            'Ergebnis',
+            style: kAppBarTextStyle,
+          ),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF55B945),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  Container(
+                    width: 220,
+                    height: 220,
+                    decoration: BoxDecoration(
+                      color: kBackgroundColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  Text(
+                    '20.1',
+                    style: TextStyle(
+                      fontSize: 90,
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                'Normal 18.5 - 25',
+                style: TextStyle(fontSize: 40),
+              )
+            ],
+          ),
+        ));
   }
 }
